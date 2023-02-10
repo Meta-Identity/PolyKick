a7790169fa7ee979614b59b2e5f5fb791016cd18
Meta-Identity <95984708+Meta-Identity@users.noreply.github.com>
2023-02-10 12:27:46 +0200

    Update PolyKick_Factory.sol

--- Contracts/PolyKick_Factory.sol
+++ Contracts/PolyKick_Factory.sol
@@ -9,12 +9,13 @@ contract PolyKick_Factory{
 
     PolyKick_ILO private pkILO;
 
-    uint256 constant months = 30 days;
-    uint256 constant MAX_UINT = 2**256 - 1;
+    
     uint256 public projectsAllowed;
     uint256 public projectsCount;
     address public owner;
     uint256 private pID;
+    uint256 private toPolykick;
+    uint256 private toExchange;
 
     event projectAdded(uint256 ProjectID, string ProjectName, IERC20 ProjectToken, address ProjectOwner);
     event ILOCreated(address pkILO);
@@ -26,11 +27,13 @@ contract PolyKick_Factory{
         address projectOwner;
         IERC20 projectToken;
         uint8 tokenDecimals;
+        IERC20 currency;
         address ILO;
         uint256 rounds;
         uint256 totalAmounts;
         uint256 polyKickPercentage;
         bool projectStatus;
+        bool isTarget;
     }
 
     struct Currencies{
@@ -40,8 +43,7 @@ contract PolyKick_Factory{
     mapping(IERC20 => Currencies) public allowedCurrencies;
     mapping(IERC20 => bool) public isCurrency;
     mapping(IERC20 => bool) public isProject;
-    mapping(uint256 => allowedProjects) public projectsByID;
-    mapping(IERC20 => uint256) private pT;
+    mapping(IERC20 => allowedProjects) public projectsByID;
 
 /* @dev: Check if contract owner */
     modifier onlyOwner (){
@@ -67,51 +69,73 @@ contract PolyKick_Factory{
         allowedCurrencies[_currency].decimals = _decimal;
         isCurrency[_currency] = true; 
     }
-    function addProject(string memory _name, IERC20 _token, uint8 _tokenDecimals, address _projectOwner, uint256 _polyKickPercentage) external onlyOwner returns(uint256) {
+    function addProject(
+        string memory _name,
+         IERC20 _token,
+          uint8 _tokenDecimals,
+           address _projectOwner,
+            uint256 _polyKickPercentage,
+             IERC20 _currency,
+              uint256 _toPolykick,
+               uint256 _toExchange
+             ) 
+             external onlyOwner returns(uint256) {
         require(isProject[_token] != true, "Project already exist!");
+        require(isCurrency[_currency] ==true, "Not a currency");
         pID++;
-        projectsByID[pID].projectID = pID;
-        projectsByID[pID].projectName = _name;
-        projectsByID[pID].projectOwner = _projectOwner;
-        projectsByID[pID].projectToken = _token;
-        projectsByID[pID].tokenDecimals = _tokenDecimals;
-        projectsByID[pID].projectStatus = true;
+        uint8 dcml = allowedCurrencies[_currency].decimals;
+        toPolykick = _toPolykick * 10 ** dcml;
+        toExchange = _toExchange * 10 ** dcml;
+        projectsByID[_token].projectID = pID;
+        projectsByID[_token].projectName = _name;
+        projectsByID[_token].projectOwner = _projectOwner;
+        projectsByID[_token].projectToken = _token;
+        projectsByID[_token].tokenDecimals = _tokenDecimals;
+        projectsByID[_token].currency = _currency;
+        projectsByID[_token].projectStatus = true;
         isProject[_token] = true;
-        pT[_token] = pID;
-        projectsByID[pID].polyKickPercentage = _polyKickPercentage;
+        projectsByID[_token].polyKickPercentage = _polyKickPercentage;
         projectsAllowed++;
         emit projectAdded(pID, _name, _token, _projectOwner);
         return(pID);
     }
-    function projectNewRound(IERC20 _token) external onlyOwner{
-        projectsByID[pT[_token]].projectStatus = true;
+    function projectNewRound(IERC20 _token, IERC20 _currency) external onlyOwner{
+        projectsByID[_token].projectStatus = true;
+        projectsByID[_token].currency = _currency;
+        toPolykick = 0;
+        toExchange = 0;
+    }
+    function targetTo(uint256 _target, uint256 _price, IERC20 _token) internal{
+        uint8 tDcml = projectsByID[_token].tokenDecimals;
+        uint256 trg = _target / 10 ** tDcml;
+        uint256 trPrice = trg * _price;
+        uint256 payTo = (toPolykick + toExchange);
+        require(trPrice > payTo * 2, "Target does not cover payments");
+        projectsByID[_token].isTarget = true;
     }
     function startILO(
         IERC20 _token, 
-        uint256 _tokenAmount, 
-        IERC20 _currency, 
+        uint256 _tokenAmount,
         uint256 _price, 
         uint8 _priceDecimals, 
         uint256 _target,
-        uint256 _months
-        ) external{
+        uint256 _days
+        ) external onlyOwner{
+            IERC20 _currency = projectsByID[_token].currency;
         require(isProject[_token] == true, "Project is not allowed!");
-        require(projectsByID[pT[_token]].projectStatus == true, "ILO was initiated");
+        require(projectsByID[_token].projectStatus == true, "ILO was initiated");
         require(_token.balanceOf(msg.sender) >= _tokenAmount,"Not enough tokens");
-        require(isCurrency[_currency] ==true, "Currency is not allowed!");
-        require(_priceDecimals <= allowedCurrencies[_currency].decimals, "Decimals error!");
-        projectsByID[pT[_token]].projectStatus = false;
-        uint256 _pkP = projectsByID[pT[_token]].polyKickPercentage;
-        address _polyKick = owner;
-        _months = _months * months;
-        uint8 priceDecimals = allowedCurrencies[_currency].decimals - _priceDecimals;
-        uint256 price = _price*10**priceDecimals;
-        uint8 _tokenDecimals = projectsByID[pT[_token]].tokenDecimals;
-        uint256 _duration = _months + block.timestamp;
-
+        require(_priceDecimals <= allowedCurrencies[_currency].decimals, "Decimal err");
+        projectsByID[_token].projectStatus = false;
+        uint256 _pkP = projectsByID[_token].polyKickPercentage;
+        uint256 price = _price*10**(allowedCurrencies[_currency].decimals - _priceDecimals);
+        uint8 _tokenDecimals = projectsByID[_token].tokenDecimals;
+        uint256 _duration = (_days * 1 /*days*/) + block.timestamp;
+        targetTo(_target, price, _token);
+        require(projectsByID[_token].isTarget == true, "is target!");
         pkILO = new PolyKick_ILO(
-            msg.sender, 
-            _polyKick, 
+            projectsByID[_token].projectOwner, 
+            owner, 
             _token,
             _tokenDecimals, 
             _tokenAmount,
@@ -119,7 +143,9 @@ contract PolyKick_Factory{
             price,
             _target, 
             _duration,
-            _pkP
+            _pkP,
+            toPolykick,
+            toExchange
             );
         emit ILOCreated(address(pkILO));
         _token.transferFrom(msg.sender, address(pkILO), _tokenAmount);
@@ -127,12 +153,12 @@ contract PolyKick_Factory{
         registerILO(_token, _tokenAmount);
     }
     function registerILO(IERC20 _token, uint256 _tokenAmount) internal{
-        projectsByID[pT[_token]].rounds++;
-        projectsByID[pT[_token]].totalAmounts += _tokenAmount;
-        projectsByID[pT[_token]].ILO = address(pkILO);
+        projectsByID[_token].rounds++;
+        projectsByID[_token].totalAmounts += _tokenAmount;
+        projectsByID[_token].ILO = address(pkILO);
     }
 }
 
                /*********************************************************
-                  Proudly Developed by MetaIdentity ltd. Copyright 2022
+                  Proudly Developed by MetaIdentity ltd. Copyright 2023
                **********************************************************/
